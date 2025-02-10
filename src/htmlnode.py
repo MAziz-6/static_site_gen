7989b206b3fc5a9ff78f0741d9c16f5531e11134
MAziz-6 <matt.aziz6@gmail.com>
2025-02-10 15:47:05 -0800

    test suite complete for htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,9 +11,9 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        if self.props != None:
-            for key, val in self.props:
-                return f" {key}{val}"
-    
+        if self.props is None:
+            return ""
+        return f' {" ".join([f'{k}="{v}"' for k, v in self.props.items()])}'
+        
     def __repr__(self):
         return f"tag:{self.tag}, value:{self.value}, children:{self.children}, props:{self.props}"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,3 +1,18 @@
 import unittest
 from htmlnode import *
 
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html(self):
+        node = HTMLNode()
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href":"https://www.google.com", "target": "_blank"})
+        expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
+
+    def test_repr(self):
+        node = HTMLNode(tag="p", value="value", children=['child1', 'child2'],props=None)
+        expected = f"tag:p, value:value, children:['child1', 'child2'], props:{None}"
+        self.assertEqual(repr(node), expected)

bc0d125e1d6362ca96638100b9ef4b38788c0de7
MAziz-6 <matt.aziz6@gmail.com>
2025-02-10 09:17:32 -0800

    First lesson complete with test done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from textnode import *
 
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node) 
 
-print("hello world")
+if __name__ == "__main__":
+    main()

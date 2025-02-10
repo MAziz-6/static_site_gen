8fdb6ae5f61aa95589fd2e4364466cada252eb0a
MAziz-6 <matt.aziz6@gmail.com>
2025-02-10 15:19:46 -0800

    First pass at htmlnode classes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,19 @@
 from enum import Enum
 
 class HTMLNode:
-    def __init__(self, tag, value, children, props):
-        
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props != None:
+            for key, val in self.props:
+                return f" {key}{val}"
+    
+    def __repr__(self):
+        return f"tag:{self.tag}, value:{self.value}, children:{self.children}, props:{self.props}"

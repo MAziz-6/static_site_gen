2d364746f7ba3b00e9d3bcff73d98cf4eb939fce
MAziz-6 <matt.aziz6@gmail.com>
2025-02-11 11:18:32 -0800

    quick syntax update for htmlnod.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,7 +20,7 @@ class HTMLNode:
     
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, children=None, props)
+        super().__init__(tag, value, children=None, props=props)
         
 
     def to_html(self):
@@ -32,4 +32,3 @@ class LeafNode(HTMLNode):
             return f"<{self.tag}>{self.value}</{self.tag}>"
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        

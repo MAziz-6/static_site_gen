62150b79c0dc0c8d23fb8f8d92d8eac2e1ddbf9f
MAziz-6 <matt.aziz6@gmail.com>
2025-02-12 20:29:28 -0800

    finalized test suite for splitnodes.py

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -5,7 +5,19 @@ from htmlnode import HTMLNode
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     childlist = []
     for node in old_nodes:
+        # Handles non text types to start
+        if node.text_type != TextType.TEXT:
+            childlist.append(node)
+            continue
+        
+        # split based on delim
         nl = node.text.split(delimiter)
+
+        # Check for unmatched delims
+        if len(nl) % 2 == 0: 
+            raise ValueError(f"Unmatched delimiter in text: {node.text}")
+        
+        # Alternating text_type for list
         toggle_type = TextType.TEXT     # Start with text type
         for segment in nl:
             childlist.append(TextNode(segment, toggle_type))
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -2,18 +2,6 @@ import unittest
 from textnode import TextNode, TextType
 from splitnodes import split_nodes_delimiter
 
-"""
-This Input:
-node = TextNode("This is text with a `code block` word", TextType.TEXT)
-new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
-Gives this Output:
-[
-    TextNode("This is text with a ", TextType.TEXT),
-    TextNode("code block", TextType.CODE),
-    TextNode(" word", TextType.TEXT),
-]
-"""
 
 class Test_Split_Nodes(unittest.TestCase):
     def test_baseline(self):
@@ -24,4 +12,33 @@ class Test_Split_Nodes(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.TEXT),
         ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_no_delim(self): 
+        node = TextNode("This is plain text without code", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [TextNode("This is plain text without code", TextType.TEXT)]
+        self.assertEqual(new_nodes, expected)
+
+    def test_multiple_of_same_delim(self):
+        node = TextNode("`code1` and `code2`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("",TextType.TEXT),
+            TextNode("code1", TextType.CODE),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("code2", TextType.CODE),
+            TextNode("", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_unmatched_delim(self):
+        node = TextNode("This is `unfinished code", TextType.TEXT)
+        with self.assertRaises(ValueError):  
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_non_text_type(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [TextNode("This is bold", TextType.BOLD)]  
         self.assertEqual(new_nodes, expected)

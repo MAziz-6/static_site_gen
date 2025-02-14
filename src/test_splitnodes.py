4238f120caa356e8630e23b9d80a4858b4e0d70d
MAziz-6 <matt.aziz6@gmail.com>
2025-02-14 13:32:15 -0800

    Finished test suite for split nodes link

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from splitnodes import split_nodes_delimiter
+from splitnodes import *
 
 
 class Test_Split_Nodes(unittest.TestCase):
@@ -42,3 +42,47 @@ class Test_Split_Nodes(unittest.TestCase):
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         expected = [TextNode("This is bold", TextType.BOLD)]  
         self.assertEqual(new_nodes, expected)
+
+class Test_Split_Nodes_Link(unittest.TestCase):
+    def test_no_links(self):
+        input_nodes = [TextNode("This is plain text.", TextType.TEXT)]
+        expected = [TextNode("This is plain text.", TextType.TEXT)]
+        result = split_nodes_link(input_nodes)
+        self.assertEqual(result, expected)
+
+    def test_single_link(self):
+        input_nodes = [TextNode("Visit [Boot.dev](https://www.boot.dev).", TextType.TEXT)]
+        expected = [
+            TextNode("Visit ", TextType.TEXT),
+            TextNode("Boot.dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(".", TextType.TEXT)
+        ]
+        result = split_nodes_link(input_nodes)
+        self.assertEqual(result, expected)
+
+    def test_multiple_links(self):
+        input_nodes = [TextNode("Links: [One](https://link1.com) and [Two](https://link2.com).", TextType.TEXT)]
+        expected = [
+            TextNode("Links: ", TextType.TEXT),
+            TextNode("One", TextType.LINK, "https://link1.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("Two", TextType.LINK, "https://link2.com"),
+            TextNode(".", TextType.TEXT)
+        ]
+        result = split_nodes_link(input_nodes)
+        self.assertEqual(result, expected)
+
+    def test_empty_text(self):
+        input_nodes = [TextNode("", TextType.TEXT)]
+        expected = [TextNode("", TextType.TEXT)]
+        result = split_nodes_link(input_nodes)
+        self.assertEqual(result, expected)
+
+    def test_adjacent_links(self):
+        input_nodes = [TextNode("[One](https://link1.com)[Two](https://link2.com)", TextType.TEXT)]
+        expected = [
+            TextNode("One", TextType.LINK, "https://link1.com"),
+            TextNode("Two", TextType.LINK, "https://link2.com"),
+        ]
+        result = split_nodes_link(input_nodes)
+        self.assertEqual(result,expected)

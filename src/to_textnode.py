b8506f26b027a897faac16252647ffda49abdc5c
MAziz-6 <matt.aziz6@gmail.com>
2025-02-15 09:24:18 -0800

    Created Text_To_nodes method in to_textnode.py

---
+++ src/to_textnode.py
@@ -0,0 +1,19 @@
+from splitnodes import *
+from textnode import *
+
+def text_to_textnodes(text): 
+    node = TextNode(text, TextType.TEXT)
+
+    # Process images and links first
+    new_nodes = split_nodes_image([node])
+    new_nodes = split_nodes_link(new_nodes)
+
+    # Process Remaining Text Types:
+    # Bold
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    # Italic
+    new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+    # Code
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+
+    return new_nodes

e90d22e1d28fcf59e7de2df6391854a4ef74062f
MAziz-6 <matt.aziz6@gmail.com>
2025-02-19 16:58:36 -0800

    Cleaned up main, good reset point for testing

--- src/main.py
+++ src/main.py
@@ -2,55 +2,63 @@ from textnode import *
 import os
 import shutil
 
-def main():
-    this_location = os.path.abspath(__file__)
-    this_dir = os.path.dirname(__file__)
-    destination = os.path.normpath(os.path.join(this_dir,"..","public"))
-    source_location = os.path.normpath(os.path.join(this_dir, "..", "static"))
-    deletion_location = os.path.normpath(os.path.join(this_dir, "..", "test"))
-    from_here = os.path.normpath(os.path.join(this_dir, "..", "from_here"))
-    to_here = os.path.normpath(os.path.join(this_dir, "..", "to_here")) 
+# this_location = os.path.abspath(__file__)
+this_dir = os.path.dirname(__file__)
+destination = os.path.normpath(os.path.join(this_dir,"..","public"))
+source_location = os.path.normpath(os.path.join(this_dir, "..", "static"))
+# deletion_location = os.path.normpath(os.path.join(this_dir, "..", "test"))
+# from_here = os.path.normpath(os.path.join(this_dir, "..", "from_here"))
+# to_here = os.path.normpath(os.path.join(this_dir, "..", "to_here")) 
 
-    """
-    For the sake of testing this section is just made to build out the test locations
-    """
-    def test_setup(from_here, deletion_location, to_here):
-        try:
-            if not os.path.exists(from_here):
-                os.makedirs(from_here)
-            
-            if not os.path.exists(deletion_location):
-                os.makedirs(deletion_location)
+def delete_directory(deletion_location):
+    try:
+        if os.path.exists(deletion_location):
+            shutil.rmtree(deletion_location)
+            print(f"Deleted directory: {deletion_location}")
+        else:
+            print(f"Directory {deletion_location} does not exist")
+    except Exception as e:
+        print(f"Dir Deletion failed: {e}")
+        pass
 
-            if not os.path.exists(to_here):
-                os.makedirs(to_here)
-            
-            with open(os.path.join(from_here, "t1.py"),"w") as f:
-                pass
-            with open(os.path.join(from_here, "t2.py"),"w") as f:
-                pass
-        except Exception as e:
-            print(e)
-            exit()
-
-    test_setup(from_here, deletion_location, to_here)
-    """
-    End of test setup section. Delete when this is done.
-    """      
-
-    # Delete old destination
-    shutil.rmtree(deletion_location)        # Deletion works
-    # Copy everything from the source location
-    for item in os.listdir(from_here):
-        source_path = os.path.join(from_here, item)
-        destination_path = os.path.join(to_here, item)
+def copy_contents(src, dest):
+    for item in os.listdir(src):
+        source_path = os.path.join(src, item)
+        destination_path = os.path.join(dest, item)
         if os.path.isfile(source_path):
             shutil.copy(source_path, destination_path)
             print(f"Copied file: {source_path} and moved to {destination_path}")
-        elif os.path.isdir(source_location):
+        elif os.path.isdir(source_path):
             shutil.copytree(source_path, destination_path)
             print(f"Copied dir: {source_path} and moved to {destination_path}")
     print(f"Everything from {source_path} has been copied to {destination_path}")
+    return
+
+def test_setup(from_here, deletion_location, to_here):
+    try:
+        if not os.path.exists(from_here):
+            os.makedirs(from_here)
+        
+        if not os.path.exists(deletion_location):
+            os.makedirs(deletion_location)
+
+        if not os.path.exists(to_here):
+            os.makedirs(to_here)
+        
+        with open(os.path.join(from_here, "t1.py"),"w") as f:
+            pass
+        with open(os.path.join(from_here, "t2.py"),"w") as f:
+            pass
+    except Exception as e:
+        print(e)
+        exit()
+
+
+def main():
+    # First, delete the destination directory
+    delete_directory(destination)
+    # Next, create/copy everything from soure to new destination dir
+    copy_contents(source_location, destination)
 
 if __name__ == "__main__":
     main()
